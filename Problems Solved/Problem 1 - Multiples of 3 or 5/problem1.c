f0b9c3fe4e0b90581a34ddf2d04e797ecf8a2fc3
Mateus Elias <meap@cin.ufpe.br>
2021-10-01 13:40:25 -0300

    Problem 1 solution added

---
+++ Problems Solved/Problem 1 - Multiples of 3 or 5/problem1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+
+    int sum = 0;
+
+    for(int i = 1; i < 1000; i++){
+        if((i%3 == 0) || (i%5 == 0)){
+            sum += i;
+        }
+    }
+
+    printf("%d", sum);
+
+    return 0;
+}
